e04afd7e2760f109e408850041977db361633b3c
Farid Ali <f.ali20@gmail.com>
2013-05-30 12:28:15 -0400

    Updated profile and added scoped status updates to it

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -16,5 +16,9 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
   	get '/register'
   	assert_response :success
   end
-  
+
+  test "that a profile page works" do
+    get '/farid'
+    assert_response :success
+  end  
 end
