51aaff57e67ec0bf1b47c162193492ab033c0df8
Farid Ali <f.ali20@gmail.com>
2013-06-17 15:58:43 -0400

    After adding slide, scrolls to bottom

--- app/controllers/slides_controller.rb
+++ app/controllers/slides_controller.rb
@@ -1,10 +1,27 @@
 class SlidesController < ApplicationController
 before_filter :authenticate_user!
 
+    def index
+    @slides = Slides.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @slides }
+    end
+  end
+
 	  def create
     @status = Status.find(params[:status_id])
     @slide = @status.slides.create(params[:slide])
-    redirect_to status_path(@status)
+       respond_to do |format|
+      if @slide.save
+        format.html { redirect_to (status_path(@status, :anchor => "form")) }
+        format.json { render json: @status, slide: :created, location: @status }
+      else
+        format.html { redirect_to @status, alert: 'Contents of slide must be between 1 - 200 characters long.' }
+        format.json { render json: @slide.errors, slide: :unprocessable_entity }
+      end
+    end
    end
 
    	  def destroy
--- app/models/slide.rb
+++ app/models/slide.rb
@@ -1,4 +1,7 @@
 class Slide < ActiveRecord::Base
   attr_accessible :content
   belongs_to :status
+
+  validates :content, presence: true,
+  					  length: { minimum: 1, maximum: 200 }
 end
